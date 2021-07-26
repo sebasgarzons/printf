8b0d866eb44ccd99cbeb1c68ce36fa3710c0f7d0
Cristian Granada <Chrisgrvc@gmail.com>
2021-07-26 17:40:34 -0500

    add edge case %%% in edge_cases.c

--- edge_cases.c
+++ edge_cases.c
@@ -14,17 +14,17 @@ int edge_cases(const char *format, va_list edge)
 	if (format == NULL)
 		return (-1);
 
-	// for (i = 0; format[i]; i++)
-	// {
-	// 	if (format[i] == '%')
-	// 	{
-	// 		i++;
-	// 		if (format[i] == '%' && format[i + 1] == '%')
-	// 		{
-	// 			return (-1);
-	// 		}
-	// 	}
-	// }
+	for (i = 0; format[i]; i++)
+	{
+		if (format[i] == '%')
+		{
+			i++;
+			if (format[i] == '%' && format[i + 1] == '%')
+			{
+				return (-1);
+			}
+		}
+	}
 	(void)edge;
 	(void)format;
 	return (0);
