4342c8b7927824dfa34b2020114e8df550255dff
LauraAlejandra2021 <85528940+LauraAlejandra2021@users.noreply.github.com>
2021-07-26 16:20:46 -0500

    intentar %d %i %o %%

--- main.c
+++ main.c
@@ -10,20 +10,20 @@
 
 int main(void)
 {
-	// int len;
-	// int len2;
-	// unsigned int ui;
+	int len;
+	int len2;
+	unsigned int ui;
 
-	// ui = (unsigned int)INT_MAX + 1024;
+	ui = (unsigned int)INT_MAX + 1024;
 
-	// len = _printf("Let's try to printf a simple sentence.\n");
-	// len2 = printf("Let's try to printf a simple sentence.\n");
-	// _printf("Length:[%d, %i]\n", len, len);
-	// printf("Length:[%d, %i]\n", len2, len2);
-	// _printf("Negative:[%d]\n", -762534);
-	// printf("Negative:[%d]\n", -762534);
-	// _printf("Unsigned octal:[%o]\n", ui);
-	// printf("Unsigned octal:[%o]\n", ui);
+	len = _printf("Let's try to printf a simple sentence.\n");
+	len2 = printf("Let's try to printf a simple sentence.\n");
+	_printf("Length:[%d, %i]\n", len, len);
+	printf("Length:[%d, %i]\n", len2, len2);
+	_printf("Negative:[%d]\n", -762534);
+	printf("Negative:[%d]\n", -762534);
+	_printf("Unsigned octal:[%o]\n", ui);
+	printf("Unsigned octal:[%o]\n", ui);
 	_printf("%%%\n");
 	printf("%%%\n");
 	return (0);
