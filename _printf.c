ddaf5c921cdc6db07be9f148b1833dcc5c79447d
Cristian Granada Valencia <52107412+Crisgrva@users.noreply.github.com>
2021-07-26 16:37:00 -0500

    update main 4:36pm

---
+++ _printf.c
@@ -0,0 +1,38 @@
+#include "holberton.h"
+
+/**
+ * _printf - function that produces output
+ * according to a format.
+ * @format: is a character string.
+ * Return: the number of characters printed
+ */
+
+int _printf(const char *format, ...)
+{
+	va_list ap;
+
+	int i, j, a;
+	int counter = 0;
+
+	va_start(ap, format);
+	a = edge_cases(format, ap);
+	if (a < 0)
+		return (-1);
+
+	for (i = 0; format[i]; i++)
+	{
+		if (format[i] == '%')
+		{
+			i++;
+			for (j = 0; j < 7; j++)
+			{
+				if (format[i] == *(types[j].name))
+					counter += types[j].f(ap);
+			}
+		}
+		else
+			counter += write(1, &format[i], 1);
+	}
+	va_end(ap);
+	return (counter);
+}
---
+++ edge_cases.c
@@ -0,0 +1,25 @@
+#include "holberton.h"
+#include <stdio.h>
+
+int edge_cases(const char *format, va_list edge)
+{
+	int i;
+
+	if (format == NULL)
+		return (-1);
+
+	for (i = 0; format[i]; i++)
+	{
+		if (format[i] == '%')
+		{
+			i++;
+			if (format[i] == '%' && format[i + 1] == '%')
+			{
+				return (-1);
+			}
+		}
+	}
+	(void)edge;
+	(void)format;
+	return (0);
+}
