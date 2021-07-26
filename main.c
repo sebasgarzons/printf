3598e421e15582f6c48274fa76dd9e8899c8815b
LauraAlejandra2021 <85528940+LauraAlejandra2021@users.noreply.github.com>
2021-07-26 16:40:10 -0500

    update main

---
+++ main.c
@@ -0,0 +1,29 @@
+#include "holberton.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <limits.h>
+
+/**
+ * main - asd
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	int len;
+	int len2;
+	unsigned int ui;
+
+	ui = (unsigned int)INT_MAX + 1024;
+
+	len = _printf("Let's try to printf a simple sentence.\n");
+	len2 = printf("Let's try to printf a simple sentence.\n");
+	_printf("Length:[%d, %i]\n", len, len);
+	printf("Length:[%d, %i]\n", len2, len2);
+	_printf("Negative:[%d]\n", -762534);
+	printf("Negative:[%d]\n", -762534);
+	_printf("Unsigned octal:[%o]\n", ui);
+	printf("Unsigned octal:[%o]\n", ui);
+	_printf("%%%\n");
+	return (0);
+}
